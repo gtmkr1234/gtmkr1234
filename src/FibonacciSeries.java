3049857577be3b0d67e08a81d7ae3d3a24b9134a
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2021-09-16 23:30:29 +0530

    Implementation of array of non primitive data type

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class init{
+    int num;
+    Scanner in= new Scanner(System.in);
+    init(){
+        System.out.println("Enter how many numbers of fibonacci series you want: ");
+        num = in.nextInt();
+    }
+
+}
+public class FibonacciSeries {
+
+}
